7bdcb3be1a0d7f1d00d17492621751940480a54a
ritaperezok <riticaperez34@gmail.com>
2019-11-05 20:09:32 -0400

    reorganizando

--- coco.py
+++ coco.py
@@ -1,40 +1,47 @@
 import sys
 import random 
-class principalMenu():
+
+class Card:
+
+    #Atributos de la clase card
+    def __init__(self,value,color,symbol):
+        self.value = value
+        self.color = color
+        self.symbol = symbol
+  
+    #Envia las cartas con su valor, color y su simbolo  
+    def information(self):
+        return f"{self.value} {self.color} {self.symbol}"   
+
+class Deck:
     def __init__(self):
-        print("\nCARD GAME 3 & 2 \nChoose An Option: \n1 -- START. \n2 -- HELP. \n3 -- CLOSE")    
-        while True:
-            num = input("ANSWER: ") 
+      self.cards = []
+      self.discard_deck = []
+      self.player1 = Players()
 
-            if num == "1":
-                #Aqui debe iniciar el juego, llamando a la clase Players.
-                self.playGame = Game()
-                break
+    #Metodo encargado de llenar la baraja
+    def fillDeck(self):
+        card_list = []
+        values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        symbols = [["Red","Heart"],["Red","Diamond"],["Black","Clover"],["Black","Peak"]]
+        for s in symbols:
+            for v in values:
+                card_list.append(Card(v,s[0],s[1]) )
+        random.shuffle(card_list)
+        self.cards = card_list
+        
 
+    def sendCard(self):
+        #Toma la ultima carta de la baraja.
+        return self.cards.pop()
 
-            if num == "2":
-                #Despues de leer debe regresar al principio del menu.
-                print("\nCOMO JUGAR.\n1-Ingresa tu nombre.\n2-Ingresa tu apellido.\n3-Apareceran varias a opciones a elegir cada vez que sea su turno.\n4-Tendras la opcion de quedarte con una carta de la baraja y cambiarla por una de tu mano o descartar la carta de la baraja.\n5-En caso de que elijas cambiar la carta de la baraja, por una de la mano, aparecera tu mano:\n\t[7 Black Peak, 6 Black Peak, A Red Diamond, 2 Black Peak, 4 Black Peak]\n6-Debes elegir entre las 5 cartas que tienes, cual deseas cambiar, digitando un la posicion de una de las cartas del 1-5.\n7-Ganará aquel que tenga 2 cartas iguales de una y tres cartas iguales de otra.")
-                
-                print("\nA- Quieres regresar al menu? \nB- Cerrar el juego?")
-                while True:
-                    num2 = input("ANSWER: ")
-                    if num2 == "A":
-                        principalMenu()
-                    if num2 == "B":
-                        print("The game will close, wait a moment, please.")
-                        sys.exit()
-                    else:
-                        print("Ha digitado un caracter incorrecto.\nElija una opcion del A OR B.")
-            if num == "3":
-                #Solo presenta el mensaje de que el juego esta cerrando y se cierra.
-                print("The game will close, wait a moment, please.")
-                sys.exit()
-
-            else:
-                print("Error, a digitado un numero incorrecto.\nElija una opcion del 1-3.")
-
-
+    def discardDeck(self):
+        #Toma la ultima carta de la baraja.
+        return self.discard_deck.append(self.player1.discarHandCard())
+    
+    def showCards(self):
+        #Llenado de un String con las 5 cartas para imprimir en los datos de cada jugador
+        return self.discard_deck[len(self.discard_deck)-1].information()
 
 class Players:
     num_player = 1
@@ -42,16 +49,17 @@ class Players:
     def __init__(self):
         self.handCard = []
         self.player()
-        # self.fillHandCard()
+        
         #X(le falta algo)
         Players.num_player += 1
 
     #Llena la baraja de cada jugador 
     def fillHandCard(self, baraja):
         #Llama el objeto baraja 5 veces para llenar la mano
+        print(baraja)
         for i in range(0,5):
             self.addCard(baraja)
-
+            
     # def discarHandCard(self):
     #     return self.handCard.pop()
     def discarHandCard(self):
@@ -69,7 +77,6 @@ class Players:
         return listCard
 
     
-
     #Metodo para mostrar los datos de cada jugador
     def show(self):
         return f"{self.player_num} \n{self.name}  {self.lastName} \n{self.showCards()}"
@@ -82,18 +89,6 @@ class Players:
         self.player_num = f"Player #{Players.num_player}."
         #Esto es un ejemplo.
         # a = f"Diga su nombre, por favor :v{self.name}"
-
-
-class Card:
-  #Atributos de la clase card
-  def __init__(self,value,color,symbol):
-    self.value = value
-    self.color = color
-    self.symbol = symbol
-  
-  #Envia las cartas con su valor, color y su simbolo  
-  def information(self):
-    return f"{self.value} {self.color} {self.symbol}"   
 
 class Game:
     def __init__(self):
@@ -113,7 +108,7 @@ class Game:
         while True:
             choises = input("ANSWER: ")    
             if choises == "1":
-                self.baraja.discardDeck()
+                # self.baraja.discardDeck()
                 self.turn()
             if choises == "2":
                 #ESTO DEBO REVISARLO
@@ -159,35 +154,43 @@ class Game:
                 Ganador.append(i)
         print(Ganador)
 
-class Deck:
+
+
+class principalMenu():
     def __init__(self):
-      self.cards = []
-      self.discard_deck = []
-      self.player1 = Players()
+        print("\nCARD GAME 3 & 2 \nChoose An Option: \n1 -- START. \n2 -- HELP. \n3 -- CLOSE")    
+        while True:
+            num = input("ANSWER: ") 
 
-    #Metodo encargado de llenar la baraja
-    def fillDeck(self):
-        list = []
-        values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-        symbols = [["Red","Heart"],["Red","Diamond"],["Black","Clover"],["Black","Peak"]]
-        for s in symbols:
-            for v in values:
-                list.append(Card(v,s[0],s[1]) )
-        random.shuffle(list)
-        self.cards = list
+            if num == "1":
+                #Aqui debe iniciar el juego, llamando a la clase Players.
+                self.playGame = Game()
+                break
 
-    def sendCard(self):
-        #Toma la ultima carta de la baraja.
-        return self.cards.pop()
 
-    def discardDeck(self):
-        #Toma la ultima carta de la baraja.
-        return self.discard_deck.append(self.player1.discarHandCard())
-    
-    def showCards(self):
-        #Llenado de un String con las 5 cartas para imprimir en los datos de cada jugador
-        return self.discard_deck[len(self.discard_deck)-1].information()
-   
+            if num == "2":
+                #Despues de leer debe regresar al principio del menu.
+                print("\nCOMO JUGAR.\n1-Ingresa tu nombre.\n2-Ingresa tu apellido.\n3-Apareceran varias a opciones a elegir cada vez que sea su turno.\n4-Tendras la opcion de quedarte con una carta de la baraja y cambiarla por una de tu mano o descartar la carta de la baraja.\n5-En caso de que elijas cambiar la carta de la baraja, por una de la mano, aparecera tu mano:\n\t[7 Black Peak, 6 Black Peak, A Red Diamond, 2 Black Peak, 4 Black Peak]\n6-Debes elegir entre las 5 cartas que tienes, cual deseas cambiar, digitando un la posicion de una de las cartas del 1-5.\n7-Ganará aquel que tenga 2 cartas iguales de una y tres cartas iguales de otra.")
+                
+                print("\nA- Quieres regresar al menu? \nB- Cerrar el juego?")
+                while True:
+                    num2 = input("ANSWER: ")
+                    if num2 == "A":
+                        principalMenu()
+                    if num2 == "B":
+                        print("The game will close, wait a moment, please.")
+                        sys.exit()
+                    else:
+                        print("Ha digitado un caracter incorrecto.\nElija una opcion del A OR B.")
+            if num == "3":
+                #Solo presenta el mensaje de que el juego esta cerrando y se cierra.
+                print("The game will close, wait a moment, please.")
+                sys.exit()
+
+            else:
+                print("Error, a digitado un numero incorrecto.\nElija una opcion del 1-3.")
+
+
 principalMenu()
 # baraja = Players()
 # Deck()
